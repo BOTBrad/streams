48567527453c5b3d98d5355932e64bf478943fe9
christian w <i@hate.coffee>
2017-09-20 23:40:17 -0400

    basic random piece selection to console

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,6 +1,6 @@
 use rand::{Rng, StdRng};
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Piece {
   Left,
   Up,
@@ -10,22 +10,58 @@ pub enum Piece {
 
 pub struct PieceGen {
   rng: StdRng,
+  prev_piece: Piece,
+  prev_foot: Piece,
 }
 
 impl PieceGen {
   pub fn new() -> PieceGen {
     PieceGen {
       rng: StdRng::new().unwrap(),
+      prev_piece: Piece::Left,
+      prev_foot: Piece::Left,
     }
   }
 
   pub fn next(&mut self) -> Piece {
-    match self.rng.gen_range::<u8>(0, 4) {
-      0 => Piece::Left,
-      1 => Piece::Up,
-      2 => Piece::Down,
-      3 => Piece::Right,
+    let pieces = self.piece_vec();
+
+    // TODO: come up with a way to make this look good
+    let next_piece = pieces[self
+      .rng
+      .gen_range::<usize>(0, pieces.len())
+    ];
+
+    self.prev_piece = next_piece;
+    self.swap_feet();
+
+    next_piece
+  }
+
+  fn swap_feet(&mut self) {
+    self.prev_foot = match self.prev_foot {
+      Piece::Left => Piece::Right,
+      Piece::Right => Piece::Left,
       _ => unreachable!(),
     }
+  }
+
+  fn piece_vec(&self) -> Vec<Piece> {
+    let all_pieces = vec![
+      Piece::Left,
+      Piece::Up,
+      Piece::Down,
+      Piece::Right,
+    ];
+
+    let mut pieces = Vec::<Piece>::new();
+    for p in all_pieces.iter() {
+      if *p != self.prev_piece
+        && *p != self.prev_foot {
+        pieces.push(*p);
+      }
+    }
+
+    pieces
   }
 }
